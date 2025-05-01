7c2ada33f06b963dc37cc66c457cd9ea555cdd5e
David Damasceno <damasceno.d@hotmail.com>
2025-05-01 14:05:46 -0300

    update

--- terraform/key.tf
+++ terraform/key.tf
@@ -1,16 +1,9 @@
-resource "tls_private_key" "rsa_4096" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
+locals {
+  key_name    = file(".keys/key_name")
+  public_key  = file(".keys/public_key")
 }
-
-variable "key_name" {}
 
 resource "aws_key_pair" "key_pair" {
-  key_name   = var.key_name
-  public_key = tls_private_key.rsa_4096.public_key_openssh
-}
-
-resource "local_file" "private_key" {
-    content = tls_private_key.rsa_4096.private_key_pem
-    filename = var.key_name
+  key_name   = local.key_name
+  public_key = local.public_key
 }
--- terraform/scripts/user_data.sh
+++ terraform/scripts/user_data.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
     
-# Update with latest packages
-yum update -y
-    
-# Install Apache
-yum install -y httpd
-    
-# Enable Apache service to start after reboot
-sudo systemctl enable httpd
-    
-# Install Hello World application
-echo "Hello World !!!" > /var/www/html/index.html
-    
-# Start Apache service
-service httpd restart
+#Install docker    
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+
